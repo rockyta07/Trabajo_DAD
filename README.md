992fe82e05f4e6ba55212617f784c5d08954d196
DaniCP909 <d.corredorpadrino@gmail.com>
2023-02-09 21:49:44 +0100

    test_01
    Pruebas iniciales

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Fluff&Paws 2
+# Fluff&Paws 
 Marcia García de la Mata, Rocío Arranz Esteban y Daniel Corredor Padrino
 
 Página Web para la adopción de animales las funcionalidades de esta estarán separadas según si son publicas o privadas:
--- paginaweb/src/main/java/fluffandpaws/paginaweb/TestController.java
+++ paginaweb/src/main/java/fluffandpaws/paginaweb/TestController.java
@@ -1,16 +1,18 @@
 package fluffandpaws.paginaweb;
 
-import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
-public class GreetingController {
-    @GetMapping("/paws")
-    public String greeting(Model model){
-        model.addAttribute("name", "");
-        return "greeting_template";
+public class TestController {
+    
+    @GetMapping("/test")
+    public String testD(Model model){
+        model.addAttribute("name", "test");
+
+        return "test_template";
     }
+
 }
