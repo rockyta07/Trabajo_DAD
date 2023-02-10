57ba504d1ebe00e00537e29427d08a4afacdbc22
DaniCP909 <d.corredorpadrino@gmail.com>
2023-02-10 02:16:29 +0100

    Update clases Animal y Protectora
    Actualizadas clases "Animal" y "Protectora" con las atributos y funciones iniciales (futura mejora si es necesario) Añadidas clases "Adoptante" "Mensaje" sin completar.

--- paginaweb/src/main/java/fluffandpaws/paginaweb/entities/Protectora.java
+++ paginaweb/src/main/java/fluffandpaws/paginaweb/entities/Protectora.java
@@ -1,7 +1,13 @@
 package fluffandpaws.paginaweb.entities;
 
-import org.springframework.stereotype.Indexed;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+//import javax.persistence.OneToOne;
+//import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
 
 @Entity
 public class Protectora {
@@ -10,10 +16,111 @@ public class Protectora {
     private long id_prt;
 
     private String name_prt;
+    private String license;
+    private String location;
+    private int tlf;
+    private String eMail;
+
+    private String social1;
+    private String social2;
+    private String social3;
+
+    private String webPrt;
+
+    private List<Animal> total;
 
     protected Protectora(){}
 
-    public Protectora(String newName){
+    public Protectora(String newName, String newLicense, String newLocation, int newTlf, String newEMail){
+        this.name_prt = newName;
+        this.license = newLicense;
+        this.location = newLocation;
+        this.tlf = newTlf;
+        this.eMail = newEMail; 
+        total = new ArrayList<Animal>();
+    }
+
+    //GETTERS
+    public String getName(){
+        return this.name_prt;
+    }
+
+    public String getLicense(){
+        return this.license;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public int getTlf(){
+        return this.tlf;
+    }
+
+    public String getEMail(){
+        return this.eMail;
+    }
+
+    public String getSocial1(){
+        return this.social1;
+    }
+
+    public String getSocial2(){
+        return this.social2;
+    }
+
+    public String getSocial3(){
+        return this.social3;
+    }
+
+    public String getWeb(){
+        return this.webPrt;
+    }
+
+    /* 
+    public ArrayList<Animal> getTotal(){
+        return (ArrayList<Animal>)this.total;
+    }
+    */
+
+    public void setName(String newName){
         this.name_prt = newName;
     }
+
+    public void setLicense(String newLicense){
+        this.license = newLicense;
+    }
+
+    public void setLocation(String newLocation){
+        this.location = newLocation;
+    }
+
+    public void setTlf(int newTlf){
+        this.tlf = newTlf;
+    }
+
+    public void setEMail(String newEMail){
+        this.eMail = newEMail;
+    }
+
+    public void setSocial1(String s1){
+        this.social1 = s1;
+    }
+
+    public void setSocial2(String s2){
+        this.social2 = s2;
+    }
+
+    public void setSocial3(String s3){
+        this.social3 = s3;
+    }
+
+    public void setWeb(String newWeb){
+        this.webPrt = newWeb;
+    }
+
+    public void adopt(Animal anm){
+        this.total.add(anm);
+    }
+
 }
