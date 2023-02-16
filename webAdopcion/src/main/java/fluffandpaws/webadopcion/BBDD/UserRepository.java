bad2c24663a0d1f39abdb9ff6c0022cea4601cc7
Ro <102540777+rockyta07@users.noreply.github.com>
2023-02-16 11:19:51 +0100

    Borrado para recarga de proyecto con BBDD

--- webAdopcion/src/main/java/fluffandpaws/webadopcion/BBDD/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package fluffandpaws.webadopcion.BBDD;
-
-import org.springframework.data.repository.CrudRepository;
-
-//Spring lo implementa automáticamente
-
-public interface UserRepository extends CrudRepository<Usuarios, Integer> {
-    //CRUD = Create, Read, Update, Delete
-}
