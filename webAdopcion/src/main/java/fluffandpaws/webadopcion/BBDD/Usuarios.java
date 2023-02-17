c29565b7ab6a7059d7e25888dce9049fa2fe5e1b
Marciagp <102741945+Marciagp@users.noreply.github.com>
2023-02-17 18:16:12 +0100

    modificacion integer por long en id

--- webAdopcion/src/main/java/fluffandpaws/webadopcion/BBDD/Usuarios.java
+++ webAdopcion/src/main/java/fluffandpaws/webadopcion/BBDD/Usuarios.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 public class Usuarios {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String name; //Nombre y apellidos
     private String username; //Nombre identificativo (generado o establecido)
@@ -31,11 +31,11 @@ public class Usuarios {
     }
 
     ////////////// Getters & Setters //////////////
-    public Integer getId(){
+    public Long getId(){
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
