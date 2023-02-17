60ce2b803f4a342c8702cb86864bd88cd5f42da9
Marciagp <102741945+Marciagp@users.noreply.github.com>
2023-02-17 18:13:20 +0100

    añadido @repository para objeto userRepository

--- webAdopcion/src/main/java/fluffandpaws/webadopcion/Repositories/UserRepository.java
+++ webAdopcion/src/main/java/fluffandpaws/webadopcion/Repositories/UserRepository.java
@@ -2,9 +2,9 @@ package fluffandpaws.webadopcion.Repositories;
 
 import fluffandpaws.webadopcion.BBDD.Usuarios;
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.stereotype.Repository;
 //Spring lo implementa automáticamente
-
+@Repository
 public interface UserRepository extends CrudRepository<Usuarios, Integer> {
     //CRUD = Create, Read, Update, Delete
 }
