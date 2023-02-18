fb70685a1ad7d47af18b97b338add75a538ea5c1
DaniCP909 <123632882+DaniCP909@users.noreply.github.com>
2023-02-18 01:26:23 +0100

    Delete webAdopcion directory

--- webAdopcion/src/main/java/fluffandpaws/webadopcion/BBDD/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package fluffandpaws.webadopcion.BBDD;
-
-import org.springframework.data.repository.CrudRepository;
-
-//Spring lo implementa automáticamente
-
-public interface UserRepository extends CrudRepository<Users, Integer> {
-    //CRUD = Create, Read, Update, Delete
-}
