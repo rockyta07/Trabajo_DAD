df00674b41387929ab13470ca033a26f60ebe15b
Marciagp <102741945+Marciagp@users.noreply.github.com>
2023-02-19 18:45:23 +0100

    update Animales

--- webAdopcion/src/main/java/fluffandpaws/webadopcion/BBDD/Animales.java
+++ webAdopcion/src/main/java/fluffandpaws/webadopcion/BBDD/Animales.java
@@ -1,47 +1,137 @@
 package fluffandpaws.webadopcion.BBDD;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 
 @Entity
 public class Animales {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String nombre;
-    private String raza;
+    private long id_anm;
 
-    public Animales(Long id,String nombre, String raza){
+    private String name_anm;
+    private short age;
 
-       this.id = id;
-       this.nombre = nombre;
-       this.raza = raza;
+    private Integer id;
+    private String species;
+    private String breed;
+    private String gender;
+    private LocalDate birthdate;
+    private short weight;
+    private short height;   //cm
 
+
+
+    @OneToOne
+    private Usuarios adopter;
+    protected Animales(){}
+
+    public Animales(String newName, short newAge, String newSp, String newBreed,
+                   String newGndr, LocalDate newBDate, short newWeight, short newHeight,Integer Id){
+        this.name_anm = newName;
+        this.age = newAge;
+        this.species = newSp;
+        this.breed = newBreed;
+        this.gender = newGndr;
+        this.birthdate = newBDate;
+        this.weight = newWeight;
+        this.height = newHeight;
+        this.adopter = null;
+        this.id = Id;
     }
-    public Long getId() {
-        return id;
+
+    //Getters
+
+    public String getName(){
+        return this.name_anm;
     }
-    public String getNombre() {
-        return nombre;
+
+    public short getAge(){
+        return this.age;
+    }
+
+    public String getSpecies(){
+        return this.species;
+    }
+
+    public String getBreed(){
+        return this.breed;
+    }
+
+    public String getGender(){
+        return this.gender;
+    }
+
+    public LocalDate getBirthdate(){
+        return this.birthdate;
+    }
+
+    public short getWeight(){
+        return this.weight;
+    }
+
+    public short getHeight(){
+        return this.height;
+    }
+
+    public Usuarios getAdopter(){
+        return this.adopter;
+    }
+
+    public Integer getId() { return this.id;}
+
+    //SETTERS
+
+    public void setName(String newName){
+        this.name_anm = newName;
     }
 
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setAge(short newAge){
+        this.age = newAge;
     }
 
-    public String getRaza() {
-        return raza;
+    public void setAge(int newAge){
+        this.age = (short)newAge;
     }
 
-    public void setRaza(String raza) {
-        this.raza = raza;
+    public void setSpecies(String newSpecies){
+        this.species = newSpecies;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setBreed(String newBreed){
+        this.breed = newBreed;
+    }
+
+    public void setGender(String newGender){
+        this.gender = newGender;
+    }
+
+    public void setBirthdate(LocalDate newBDate){
+        this.birthdate = newBDate;
+    }
+
+    public void setWeight(short newWeight){
+        this.weight = newWeight;
+    }
+
+    public void setWeight(int newWeight){
+        this.weight = (short)newWeight;
+    }
+
+    public void setHeight(short newHeight){
+        this.height = newHeight;
+    }
+
+    public void setHeight(int newHeight){
+        this.height = (short)newHeight;
+    }
+
+    public void setId(Integer id){this.id = id;}
+
+    public void setAdopter(Usuarios adp){
+        this.adopter = adp;
     }
 }
