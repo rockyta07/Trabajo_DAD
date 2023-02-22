75b02bb1ce0778d8abde474b457c5298f59d2642
Marciagp <102741945+Marciagp@users.noreply.github.com>
2023-02-22 19:48:07 +0100

    Update Messages.java

--- webAdopcion/src/main/java/fluffandpaws/webadopcion/BBDD/Messages.java
+++ webAdopcion/src/main/java/fluffandpaws/webadopcion/BBDD/Messages.java
@@ -4,48 +4,49 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Messages {
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
-        private long id_msg;
+        private Long id_msg;
 
         private String content;
 
-        private Integer id;
-        @OneToOne
-        private Usuarios adp;
+        //private Integer id;
 
+        /*
         @OneToOne
+        private Usuarios adp;*/
+
+        @ManyToOne
         private Shelter prt;
 
         protected Messages(){}
 
         public Messages(String newCont){
             this.content = newCont;
-            prt = new Shelter();
         }
 
         public String getContent(){
             return this.content;
         }
 
-        public Usuarios getAdopter(){
-            return this.adp;
-        }
+        //public Usuarios getAdopter(){
+           // return this.prt;
+        //}
 
-        public void setAdopter(Usuarios newAdp){
-            this.adp = newAdp;
-        }
+        //public void setAdopter(Usuarios newAdp){
+            //this.prt = newAdp;
+        //}
 
-        public Integer getId(){ return this.id;}
+        public Long getId(){ return this.id_msg;}
         public Shelter getPrt(){
             return this.prt;
         }
 
-        public void setId(Integer id){ this.id = id;}
+        public void setId(Long id){ this.id_msg = id;}
         public void setPrt(Shelter newPrt){
             this.prt = newPrt;
         }
