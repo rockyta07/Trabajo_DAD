ccae17675b71445275d7e0dcd2410b197fc5dc42
Marciagp <102741945+Marciagp@users.noreply.github.com>
2023-02-22 19:55:01 +0100

    Update MessagesController.java

--- webAdopcion/src/main/java/fluffandpaws/webadopcion/Controllers/MessagesController.java
+++ webAdopcion/src/main/java/fluffandpaws/webadopcion/Controllers/MessagesController.java
@@ -1,10 +1,7 @@
 package fluffandpaws.webadopcion.Controllers;
 
 import fluffandpaws.webadopcion.BBDD.Messages;
-import fluffandpaws.webadopcion.BBDD.Shelter;
-import fluffandpaws.webadopcion.Service.animalesService;
 import fluffandpaws.webadopcion.Service.mensaggesService;
-import fluffandpaws.webadopcion.Service.shelterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -14,43 +11,43 @@ import java.util.List;
 public class MessagesController {
 
     @Autowired
-    private mensaggesService men;
+    private mensaggesService meServ;
 
 
     @GetMapping("/")//buscamos todos los mensajes
     public List<Messages> getMessages() {
 
-        return (List<Messages>) men.findAll();
+        return (List<Messages>) meServ.findAll();
     }
 
     @GetMapping("/{id}")//Esto nos retorna el mensaje
-    public Messages getMessagges(@PathVariable Integer id) {
+    public Messages getMessagges(@PathVariable long id) {
 
-        return men.findById(id).orElseThrow();
+        return meServ.findById((int) id).orElseThrow();
     }
 
     @PostMapping("/")//creamos el mensaje
     public Messages createMessages(@RequestBody Messages me) {
 
-       men.save(me);
+        meServ.save(me);
         return me;
     }
 
     @PutMapping("/{id}")//para modificar mensaje
 
-    public Messages replaceMessages(@PathVariable Integer id, @RequestBody Messages newMessage) {
+    public Messages replaceMessages(@PathVariable long id, @RequestBody Messages newMessage) {
 
-        newMessage.setId(id);
-        men.replace(newMessage);
+        newMessage.setId(Long.valueOf(id));
+        meServ.replace(newMessage);
         return newMessage;
 
     }
     @DeleteMapping("/{id})")//borramos el mensaje
 
-    public Messages deleteMessages(@PathVariable Integer id){
+    public Messages deleteMessages(@PathVariable long id){
 
-       Messages m = men.findById(id).orElseThrow();
-       men.deleteById(Math.toIntExact(id));
+       Messages m = meServ.findById((int) id).orElseThrow();
+        meServ.deleteById(Math.toIntExact(id));
         return m;
 
 
