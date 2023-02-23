d95ffd41f71a0e147616af9bb82442d954f2f418
DaniCP909 <d.corredorpadrino@gmail.com>
2023-02-23 16:11:00 +0100

    Create shelterController.java

---
+++ webAdopcion/src/main/java/fluffandpaws/webadopcion/Controllers/shelterController.java
@@ -0,0 +1,58 @@
+package fluffandpaws.webadopcion.Controllers;
+
+import fluffandpaws.webadopcion.BBDD.Animales;
+import fluffandpaws.webadopcion.BBDD.Shelter;
+import fluffandpaws.webadopcion.Service.animalesService;
+import fluffandpaws.webadopcion.Service.shelterService;
+import fluffandpaws.webadopcion.Service.userService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/shelter")
+public class shelterController {
+
+    @Autowired
+    private shelterService shel;
+
+
+    @GetMapping("/")//buscamos todos los shelter
+    public List<Shelter> getShelter() {
+
+        return (List<Shelter>) shel.findAll();
+    }
+
+    @GetMapping("/{id}")//Esto nos retorna el shelter
+    public Shelter getShelter(@PathVariable Long id) {
+
+        return shel.findById(id).orElseThrow();
+    }
+
+    @PostMapping("/")//creamos el shelter
+    public Shelter createShelter(@RequestBody Shelter sh) {
+
+        shel.save(sh);
+        return sh;
+    }
+
+    @PutMapping("/{id}")//para modificar shelter
+
+    public Shelter replaceShelter(@PathVariable Long id, @RequestBody Shelter newShelter) {
+
+        newShelter.setId(id);
+        shel.replace(newShelter);
+        return newShelter;
+
+    }
+
+    @DeleteMapping("/{id})")//borramos el shelter
+
+    public Shelter deleteShelter(@PathVariable Long id) {
+
+        Shelter s = shel.findById(id).orElseThrow();
+        shel.deleteById(id);
+        return s;
+
+    }
+}
