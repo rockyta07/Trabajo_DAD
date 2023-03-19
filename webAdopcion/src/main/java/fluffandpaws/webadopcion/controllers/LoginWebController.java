2ec206746c9f2e4c86c57a7930c0b2e290ee3a3f
Marciagp <marciapintoo21@hotmail.com>
2023-03-20 00:01:40 +0100

    añadido toda la carpeta jwt

--- webAdopcion/src/main/java/fluffandpaws/webadopcion/controllers/LoginWebController.java
+++ webAdopcion/src/main/java/fluffandpaws/webadopcion/controllers/LoginWebController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpSession;
@@ -25,5 +26,6 @@ public class LoginWebController {
         return "loginerror";
 
     }
+
 
 }
--- webAdopcion/src/main/java/fluffandpaws/webadopcion/controllers/auth/LoginController.java
+++ webAdopcion/src/main/java/fluffandpaws/webadopcion/controllers/auth/LoginController.java
@@ -5,10 +5,7 @@ import fluffandpaws.webadopcion.security.jwt.AuthResponse;
 import fluffandpaws.webadopcion.security.jwt.LoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import fluffandpaws.webadopcion.security.jwt.UserLoginService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,7 +20,7 @@ public class LoginController {
     private UserLoginService userService;//encontrado en la carpeta service
 
 
-    @RequestMapping("/login")
+    @PostMapping("/login")
 //verificamos si lo que ha ingresado el usuario está bien para poder acceder
     public ResponseEntity<AuthResponse> login(
             @CookieValue(name = "accessToken", required = false) String accessToken,
@@ -33,14 +30,14 @@ public class LoginController {
         return userService.login(loginRequest, accessToken, refreshToken);
     }
 
-    @RequestMapping("/refresh")
+    @PostMapping("/refresh")
     public ResponseEntity<AuthResponse> refreshToken(
             @CookieValue(name = "refreshToken", required = false) String refreshToken) {
 
         return userService.refresh(refreshToken);
     }
 
-    @RequestMapping("/logout")
+    @PostMapping("/logout")
     public ResponseEntity<AuthResponse> logOut(HttpServletRequest request, HttpServletResponse response) {
 
         return ResponseEntity.ok(new AuthResponse(AuthResponse.Status.SUCCESS, userService.logout(request, response)));
