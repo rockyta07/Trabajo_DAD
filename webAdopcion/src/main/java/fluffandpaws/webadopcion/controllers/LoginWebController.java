cb622496d83dd523dc5b081c59284550218a0e30
Marciagp <102741945+Marciagp@users.noreply.github.com>
2023-03-20 00:30:20 +0100

    commit sin relevancia

--- webAdopcion/src/main/java/fluffandpaws/webadopcion/controllers/LoginWebController.java
+++ webAdopcion/src/main/java/fluffandpaws/webadopcion/controllers/LoginWebController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
-
+//Este código define un controlador web en Spring para la página de login. El controlador tiene tres métodos: login,loginerror,loginout
+//para poder iniciar sesion, cerrar y en el caso de que metas mas los credenciales
 @Controller
 
 public class LoginWebController {
@@ -25,6 +26,11 @@ public class LoginWebController {
     public String loginerror(){
         return "loginerror";
 
+    }
+    @GetMapping ("/logout")//entramos a la url de login
+
+    public String logout() {
+        return "logout";
     }
 
 
